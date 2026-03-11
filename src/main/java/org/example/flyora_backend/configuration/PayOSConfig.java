9a2e3ce9ec10b3de06006dd01a7d8244bd9996b7
Huntrot <captainparrot13042005@gmail.com>
2026-03-11 15:42:58 +0700

    fixing payOSConfig x4

--- src/main/java/org/example/flyora_backend/configuration/PayOSConfig.java
+++ src/main/java/org/example/flyora_backend/configuration/PayOSConfig.java
@@ -10,9 +10,9 @@ public class PayOSConfig {
     @Bean
     public PayOS payOS() {
         return new PayOS(
-            "c76bb3a5-fb30-4643-a33d-e36d0f18f1b2",
-            "b4be97a8-d346-4128-8494-22c8f931023f",      
-            "0505c89b495c811311f0c02ad65f4ef0efbb8a5d2e4c2556fd2ca004086207c6"
+            "b4be97a8-d346-4128-8494-22c8f931023f", // Client ID
+            "c76bb3a5-fb30-4643-a33d-e36d0f18f1b2", // API Key
+            "0505c89b495c811311f0c02ad65f4ef0efbb8a5d2e4c2556fd2ca004086207c6" // Checksum
         );
     }
 }
